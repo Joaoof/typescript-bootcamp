a30d5b4dabedd520d94dd1b40488395e28de42f1
Joaoof <joaodeus400@gmail.com>
2023-10-17 10:54:21 -0300

    String, number e booleanos

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+let message: string
+
+message = 'Hello Word'
+
+message.toLowerCase()
+
+console.log(message);
+
+let total: number
+total = 10
+
+console.log(total);
+
+let isOpen: boolean
+isOpen = false
+console.log(isOpen);
+
